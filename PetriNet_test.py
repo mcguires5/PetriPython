d4728e9240e2e5d962bc503c92f3a649104f9093
mcguires5 <mcguires5@students.rowan.edu>
2019-03-31 23:19:39 -0400

    Fixed shape of A

--- PetriNet_test.py
+++ PetriNet_test.py
@@ -1,6 +1,6 @@
 import PetriNet
 def test_nextMarking():
-    A = [[0, 0, 0], [-1, 1, 0], [0, -1, 1]]
+    A = [[0, 0, 0], [-1, 1, 0], [-1, 1, -1], [0, -1, 1]]
     M = [[1],[0],[1],[0]]
     u = [[0], [0],[1]]
     assert PetriNet.NextMarking(A, M, u) == [[1],[0],[0],[1]]
